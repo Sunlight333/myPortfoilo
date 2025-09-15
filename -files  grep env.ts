3f8b5bffeb13f02784f62117646ac712d06acee3
ivandonyk <ivandonyk@gmail.com>
2025-09-14 21:51:14 -0400

    .

---
+++ -files  grep env.ts
@@ -0,0 +1,5 @@
+[33m890a680[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m Fix Vercel deployment: Add env.ts file and update gitignore
+[33m9b1a283[m fix all
+[33maeb4e8d[m[33m ([m[1;31morigin/sunlight[m[33m, [m[1;32msunlight[m[33m)[m fixed for deployment
+[33m4ef0cde[m Add files via upload
+[33m929759c[m feat: created my portfolio
